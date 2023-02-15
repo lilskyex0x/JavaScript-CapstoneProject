1ffb77769283a7193b1aac40d3b8a1335e6b265d
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-02-15 15:50:30 +0630

    Revise to fetch API sepearately

--- src/modules/fetchApi.js
+++ src/modules/fetchApi.js
@@ -1,21 +1,9 @@
-const createFoodList = async () => {
-    const foodListElement = document.querySelector('.food-list');
-    const response = await fetch('https://themealdb.com/api/json/v1/1/search.php?s');
-    const data = await response.json();
-    data.meals.forEach(meal => {
-        foodListElement.innerHTML += `
-            <div class='grid-item'>
-                <img src="${meal.strMealThumb}" alt="${meal.strMeal}"  width="250px" height="200px">
-                <div class='small-buttons'>
-                    <p>${meal.strMeal} <i class="fa fa-heart-o"></i></p>
-                    <button>Comments</button>
-                    <br><br>
-                    <button>Reserve</button>
-                </div>
-                
-            </div>
-            <br>
-        `;
-    });
+const getMeals = async () => {
+  const response = await fetch(
+    "https://themealdb.com/api/json/v1/1/search.php?s"
+  );
+  const data = await response.json();
+  return data.meals;
 };
-export default createFoodList
+
+export { getMeals };
