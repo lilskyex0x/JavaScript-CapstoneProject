774e93e2137cd4ab8d080b746130e8f374dadeb4
Obed Bamfo <obamfo@st.ug.edu.gh>
2023-02-18 19:44:43 +0000

    Delete fetchApi.js

--- src/modules/fetchApi.js
+++
@@ -1,9 +0,0 @@
-const getMeals = async () => {
-  const response = await fetch(
-    "https://themealdb.com/api/json/v1/1/search.php?s"
-  );
-  const data = await response.json();
-  return data.meals;
-};
-
-export { getMeals };
